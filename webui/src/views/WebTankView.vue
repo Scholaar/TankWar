d0367c4be8ae4da1f8da5917e4c61b9620f1eb8e
hicircum <93832807+Hicircum@users.noreply.github.com>
2023-11-12 19:28:19 +0800

    feat: add game main page

--- webui/src/views/WebTankView.vue
+++ webui/src/views/WebTankView.vue
@@ -1,63 +1,237 @@
 <template>
-    <div class="main" @keydown="handleKeydown" tabindex="0">
-        <div v-for="(tank, index) in tankPosition" :key="tank.name" :class="['tank', index === 0 ? 'friendly' : 'enemy']" :style="{left: `${tank.x}px`, top: `${tank.y}px`}">
-            {{ tank.name }}
+    <div class="playground">
+        <div id="rectangle"
+        class="rectangle"
+        :style="{width: resolution.width + 'px', height: resolution.height + 'px'}"
+        >
+            <div
+            id="square"
+            class="square"
+            style="background: url('src/assets/tank.png');background-size: cover;"
+            :style="{
+                top: postionConvertY(tankPosition.myTank.y)+'px',
+                left: postionConvertX(tankPosition.myTank.x)+'px',
+                transform: directionHandler(tankPosition.myTank.direction),
+            }"
+            ></div>
+
+            <div
+            id="square"
+            class="square"
+            style="background: url('src/assets/tank.png');background-size: cover;background-color: red;"
+            v-for="enemyTank in tankPosition.enemyTank"
+            :style="{
+                top: postionConvertY(enemyTank.y)+'px',
+                left: postionConvertX(enemyTank.x)+'px',
+                transform: directionHandler(enemyTank.direction),
+            }"
+            ></div>
+
+            <div
+            id="square"
+            class="square"
+            style="background: url('src/assets/tank.png');background-size: cover;background-color: aqua;"
+            v-for="bullet in bullets"
+            :style="{
+                top: postionConvertY(bullet.y)+'px',
+                left: postionConvertX(bullet.x)+'px',
+                transform: directionHandler(bullet.direction),
+            }"
+            ></div>
         </div>
-        <div v-for="wall in walls" :key="wall.id" class="wall" :style="{left: `${wall.x}px`, top: `${wall.y}px`}"></div>
-        <div v-for="grass in grasses" :key="grass.id" class="grass" :style="{left: `${grass.x}px`, top: `${grass.y}px`}"></div>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 
-const tankPosition = ref([
-    {"x": 0, "y": 0, "name": 'tank1'},
-    {"x": 50, "y": 50, "name": 'tank2'},
-    {"x": 100, "y": 100, "name": 'tank3'}
-])
+const tankPosition = ref({
+    "myTank": {
+        "x": 100,
+        "y": 300,
+        "direction": "right"
+    },
+    "enemyTank": [
+        {
+            "x": 200,
+            "y": 200,
+            "direction": "up"
+        },
+        {
+            "x": 400,
+            "y": 400,
+            "direction": "down"
+        },
+        {
+            "x": 600,
+            "y": 600,
+            "direction": "left"
+        },
+        {
+            "x": 300,
+            "y": 200,
+            "direction": "right"
+        }
+    ]
+})
 
-const walls = ref([
-    {
-        x: 200,
-        y: 200,
+const bullets = ref([])
+
+const resolution = ref({
+    width: 1280,
+    height: 720
+})
+
+const directionHandler = (direction) => {
+    switch (direction) {
+        case 'up':
+            return 'rotate(' + 0 + 'deg)'
+        case 'down':
+            return 'rotate(' + 180 + 'deg)'
+        case 'left':
+            return 'rotate(' + 270 + 'deg)'
+        case 'right':
+            return 'rotate(' + 90 + 'deg)'
     }
-])
+}
 
-const grasses = ref([
-    {x: 500, y: 500, id: 1},
-    {x: 500, y: 550, id: 2},
-])
+const postionConvertX = (x) => {
+    // 将坐标转换为方格坐标,同时适应屏幕缩放，坐标为1920*1080
+    return x * resolution.value.width / 1920
+}
+
+const postionConvertY = (y) => {
+    // 将坐标转换为方格坐标,同时适应屏幕缩放，坐标为1920*1080
+    return y * resolution.value.height / 1080
+}
+
+const createBullet = (x,y,direction) => {
+    bullets.value.push({
+        "x": x,
+        "y": y,
+        "direction": direction
+    })
+}
+
+// 上下左右移动
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if(tankPosition.value.myTank.y <= 0) return;
+            tankPosition.value.myTank.y -= 10
+            tankPosition.value.myTank.direction = 'up'
+            break
+        case 'ArrowDown':
+            if(tankPosition.value.myTank.y >= 1080-80) return;
+            tankPosition.value.myTank.y += 10
+            tankPosition.value.myTank.direction = 'down'
+            break
+        case 'ArrowLeft':
+            if(tankPosition.value.myTank.x <= 0) return;
+            tankPosition.value.myTank.x -= 10
+            tankPosition.value.myTank.direction = 'left'
+            break
+        case 'ArrowRight':
+            if(tankPosition.value.myTank.x >= 1920-80) return;
+            tankPosition.value.myTank.x += 10
+            tankPosition.value.myTank.direction = 'right'
+            break
+        case ' ':
+            createBullet(tankPosition.value.myTank.x,tankPosition.value.myTank.y,tankPosition.value.myTank.direction)
+            break
+    }
+})
+
+onMounted(() => {
+    // 获取窗口宽度和高度
+    var w = window.innerWidth*0.9;
+    var h = window.innerHeight*0.9;
+    if(w/h > 16/9){
+        resolution.value.width = h * 16/9
+        resolution.value.height = h
+    }else{
+        resolution.value.width = w
+        resolution.value.height = w * 9/16
+    }
+})
+
+// 监听窗口变化
+window.addEventListener('resize', () => {
+    var w = window.innerWidth*0.9;
+    var h = window.innerHeight*0.9;
+    if(w/h > 16/9){
+        resolution.value.width = h * 16/9
+        resolution.value.height = h
+    }else{
+        resolution.value.width = w
+        resolution.value.height = w * 9/16
+    }
+})
+
+setInterval(() => {
+    bullets.value.forEach((bullet,index) => {
+        switch (bullet.direction) {
+            case 'up':
+                bullet.y -= 10
+                break
+            case 'down':
+                bullet.y += 10
+                break
+            case 'left':
+                bullet.x -= 10
+                break
+            case 'right':
+                bullet.x += 10
+                break
+        }
+        if(bullet.x < 0 || bullet.x > 1920-80 || bullet.y < 0 || bullet.y > 1080-80){
+            bullets.value.splice(index,1)
+        }
+    })
+}, 50)
+
+// 创建websocket连接，对坦克位置和子弹位置进行赋值
+// const ws = new WebSocket('ws://localhost:8080')
+// ws.onopen = () => {
+//     console.log('ws open')
+// }
+// ws.onmessage = (e) => {
+//     console.log(e.data)
+//     let data = JSON.parse(e.data)
+//     tankPosition.value = data.tankPosition
+//     bullets.value = data.bullets
+// }
+// ws.onclose = () => {
+//     console.log('ws close')
+// }
+watch(tankPosition.value.myTank, (newValue, oldValue) => {
+    // ws.send(JSON.stringify({
+    //     "tankPosition": newValue,
+    //     "bullets": bullets.value
+    // }))
+    // console.log(newValue)
+})
 </script>
 
 <style lang="less" scoped>
-.tank {
-    position: absolute;
-    width: 50px;
-    height: 50px;
-    text-align: center;
-    line-height: 50px;
-}
-
-.friendly {
-    background-color: blue;
-}
-
-.enemy {
-    background-color: red;
-}
-
-.wall {
-    position: absolute;
-    width: 50px;
-    height: 50px;
-    background-color: gray;
-}
-
-.grass {
-    position: absolute;
-    width: 50px;
-    height: 50px;
-    background-color: green;
+.playground{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    .rectangle {
+        position: relative;
+        background-color: #d5d5d5;
+        border: 5px solid rgb(72, 255, 0);
+        .square {
+            position: absolute;
+            width: calc(100% / 24);
+            height: calc(100% / 13.5);
+            box-sizing: border-box;
+            border: 1px solid red;
+        }
+    }
 }
 </style>
